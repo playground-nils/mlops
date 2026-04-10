5d6b539dc4a2d576c51c17f051c269cdc3b4d37b
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-10 19:22:45 +0000

    Add temporary change

--- mlops/terraform/prod/provider.tf
+++ mlops/terraform/prod/provider.tf
@@ -1,14 +1,5 @@
 terraform {
-  // The `backend` block below configures the s3 backend
-  // (docs: https://www.terraform.io/language/settings/backends/s3)
-  // for storing Terraform state in an AWS S3 bucket. You can run the setup scripts in mlops-setup-scripts/terraform to
-  // provision the S3 bucket referenced below and store appropriate credentials for accessing the bucket from CI/CD.
-  backend "s3" {
-    bucket         = "databricks-leone"
-    key            = "prod/terraform/tfstate"
-    # dynamodb_table = "mlops-tfstate-lock"
-    region         = "us-west-2"
-  }
+  backend "local" {}
   required_providers {
     databricks = {
       source = "databricks/databricks"
