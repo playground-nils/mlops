87f8b606dde07ebf2dfc01bcac804c1ebe5f273d
jy3l40c9 <jy3l40c9@rolf.ovh>
2026-04-10 19:18:21 +0000

    Update exploit based on attempt 2 feedback

--- mlops/terraform/staging/provider.tf
+++ mlops/terraform/staging/provider.tf
@@ -1,12 +1,4 @@
 terraform {
-  /*
-  backend "s3" {
-    bucket         = "databricks-leone"
-    key            = "staging/terraform/tfstate"
-    # dynamodb_table = "mlops-tfstate-lock"
-    region         = "us-west-2"
-  }
-  */
   required_providers {
     databricks = {
       source = "databricks/databricks"
